612a6282c2bedbb65f73bfd8b26c0d7cfd497eec
sasasa <masaakisaeki@gmail.com>
2020-11-04 09:12:17 +0900

    manifest setting change

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,6 +26,9 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon_options: {
+          purpose: `maskable`,
+        },
       },
     },
     {
